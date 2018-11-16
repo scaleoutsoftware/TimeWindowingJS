a974c1e4e8362188270de3fd215d1dc8c2b4a7b8
markwaterman <markwaterman@users.noreply.github.com>
2018-11-16 14:00:38 -0800

    Initial project commit.

---
+++ index.js
@@ -0,0 +1,25 @@
+
+function addToOrdered(arr, timestampSelector, ...values) {
+    if (!arr.isArray()) {
+        throw new TypeError('arr must be an Array instance');
+    }
+    if (timestampSelector == null) {
+        throw new TypeError('timestampSelector cannot be null/undefined');
+    }
+
+    for (let i = 0; i < values.length; i++) {
+        const timestamp = timestampSelector(values[i]);
+        let insertPosition = arr.length;
+        while (insertPosition > 0) {
+            if (timestamp < timestampSelector(arr[insertPosition - 1]))
+                insertPosition--;
+            else
+                break;
+        }
+        arr.splice(insertPosition, 0, values[i]);
+    }
+}
+
+module.exports = {
+    addToOrdered: addToOrdered,
+};
