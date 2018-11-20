d4fe6e9f169cd97d35d88486a40cc9b9aa74d012
Mark Waterman <markwaterman@users.noreply.github.com>
2018-11-19 16:41:24 -0800

    signatures for addtional add methods

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
  */
 
 /**
+ * Adds one or more elements to a time-ordered array of items, inserting them in chronological order.
  * @param {Array} arr - destination array for new element(s).
  * @param {TimestampSelector} timestampSelector - function to extract a timestamp from an element.
  * @param {...any} values - value(s) to add to the array.
@@ -37,6 +38,42 @@ function addToOrdered(arr, timestampSelector, ...values) {
     }
 }
 
+/**
+ * Adds one or more elements to a time-ordered array of items, inserting them in chronological order. If
+ * the size of the destination array exceeds the supplied maxArraySize argument, the oldest elements in
+ * the destination array will be evicted.
+ * @param {Array} arr - destination array for new element(s).
+ * @param {number} maxArraySize - max allowed size of destination array before eviction begins.
+ * @param {TimestampSelector} timestampSelector - function to extract a timestamp from an element.
+ * @param {...any} values - value(s) to add to the array.
+ */
+function addToOrderedAndEvictOldest(arr, maxArraySize, timestampSelector, ...values) {
+    if (!Number.isInteger(maxArraySize) || maxArraySize <= 0) {
+        throw new RangeError('maxArraySize must be a positive integer.');
+    }
+
+    addToOrdered(arr, timestampSelector, ...values);
+    
+    
+}
+
+/**
+ * Adds one or more elements to a time-ordered array of items, inserting them in chronological order. Any
+ * elements in the array with timestamp prior to the supplied startTime argument will be evicted from the 
+ * destination array.
+ * @param {Array} arr - destination array for new element(s).
+ * @param {Date|number} startTime - start time (inclusive) of the first allowed element in the destination array.
+ * @param {TimestampSelector} timestampSelector - function to extract a timestamp from an element.
+ * @param {...any} values - value(s) to add to the array.
+ */
+function addToOrderedAndEvictBefore(arr, startTime, timestampSelector, ...values) {
+    addToOrdered(arr, timestampSelector, ...values);
+    
+    
+}
+
 module.exports = {
     addToOrdered: addToOrdered,
+    addToOrderedAndEvictOldest: addToOrderedAndEvictOldest,
+    addToOrderedAndEvictBefore: addToOrderedAndEvictBefore
 };
